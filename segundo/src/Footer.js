c0073ffd2acc2a2f9bcf361c87952b4f5d942c83
FERNANDO MIRANDA DA SILVA SANTOS <fernando.mssantos2@senacsp.edu.br>
2022-05-11 16:56:19 -0300

    Segundo Projeto

---
+++ segundo/src/Footer.js
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <div className="footer">
+            <h3>Consulte a Programação Completa em <br></br>
+                <a href="http://www.sp.senac.br/tatuape"> sp.senac.br/tatuape </a> 
+                Escaneie o QR Code
+            </h3>
+        </div>
+    )
+}
+export default Footer
---
+++ segundo/src/Main.js
@@ -0,0 +1,28 @@
+function Main(){
+    return(
+        <div className="main">
+            <h4>Bem-Estar</h4>
+             <ul>
+                 <li>Reiki-Master</li>
+                 <li>Reiki-Nível 1 e 2</li>
+                 <li>Reiki-Nível 3</li>
+             </ul>
+
+             <h4>Gestão e Negócios</h4>
+             <ul>
+                 <li>Administração Financeira</li>
+                 <li>Analista Final</li>
+                 <li>Contabilidade Geral</li>
+             </ul>
+
+             <h4>Tecnologia da Informação</h4>
+             <ul>
+                 <li>Autocad - Projetos em 2D</li>
+                 <li>Excel 2019</li>
+                 <li>Excel 2019 - Avançado</li>
+             </ul>
+             <img className="img" alt="imagem reiki" src="https://images.unsplash.com/photo-1638913662584-731da41f5a59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"></img>
+        </div>
+    )
+}
+export default Main;
