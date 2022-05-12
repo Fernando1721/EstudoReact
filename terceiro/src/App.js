47fed6c11f9bd713f8347a0fac8d8df0aeaaf928
FERNANDO MIRANDA DA SILVA SANTOS <fernando.mssantos2@senacsp.edu.br>
2022-05-12 17:26:34 -0300

    Criação de estado com objetos

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,16 +1,65 @@
 import {useState} from "react";
 function App() {
  
-  const [numero, setNumero] = useState(1);
+  const [produto, setProduto] = useState(
+    [
+      {
+        id:500,
+        nome:"Mouse",
+        categoria:"informática",
+        preco:50.50
+      },
+      {
+        id:501,
+        nome:"Teclado",
+        categoria:"informática",
+        preco:150.90
+      }
+    ]
+  );
 
   return (
     <div>
-     <p>O número atual é {numero}</p>
-     <p>
-       <button title="Clique" onClick={() => setNumero(numero+1)}>Clique Aqui</button>
-     </p>
+     <button 
+     onClick={() => {
+       let id = prompt("Entre com o id do produto");
+       let nome = prompt("Entre com o nome do produto");
+       let categoria = prompt("Entre com a categoria do produto");
+       let preco = prompt("Entre com o preco do produto");
 
-    </div>
+      setProduto(
+        produto.concat(
+        {
+          id:id,
+          nome:nome,
+          categoria:categoria,
+          preco:preco
+        })
+      )
+     }}
+     >
+       Adicionar Produto</button>
+
+
+      
+        
+     <h2>Produto Cadastrados</h2>
+
+
+     {produto.map((item,index) => (
+       
+        <div key={index}>
+
+       <h3>{item.nome}</h3>
+       <ul>
+         <li>Código do Produto: {item.id}</li>
+         <li>Categoria do produto: {item.categoria}</li>
+         <li>Preço do Produto: {item.preco}</li>
+       </ul>
+        </div>
+
+     ))}
+      </div>
   );
 }
 
