1df17e72453cfa6b809cd328dea9f88a75f38844
FERNANDO MIRANDA DA SILVA SANTOS <fernando.mssantos2@senacsp.edu.br>
2022-05-17 17:22:59 -0300

    Criação da API e realização do react

---
+++ quarto/src/components/main/Container.js
@@ -0,0 +1,39 @@
+import {useState,useEffect} from "react";
+
+import Mensagem from "./Mensagem"
+import Conteudo from "./Conteudo"
+
+export default function Container(){
+
+    //vamos o estado inicial dos produtos, ou seja,
+    //quais dados existem dentro do objeto produtos
+
+    const[produto,setProdutos] = useState([
+        {
+            id:"",
+            nome:"",
+            descricao: "",
+            categoria: "",
+            preco: "",
+            msg:[]
+        }
+    ]);
+
+    useEffect(()=>{
+
+        fetch("http://10.26.49.30:4000/api/services/produto/listar")
+	.then((response)=>response.json())
+	.then((dados)=>{
+	  setProdutos(dados.output);
+	  })
+	  .catch((erro)=>console.error(`Erro ao carregar a api ->${erro}`))
+        
+    },[])
+
+    return(
+        <div className="container">
+            <Mensagem />
+            <Conteudo dados = {produto} />
+        </div>
+    )
+}
